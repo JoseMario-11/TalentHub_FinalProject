a2c9c3b9bb212af7be44e76768b651489fdb3d00
danieelfcr <danielfcab2002@gmail.com>
2022-11-09 07:52:55 -0600

    Solution test 2

--- TalentHub/TalentHub/Form1.Designer.cs
+++ TalentHub/TalentHub/Form1.Designer.cs
@@ -29,10 +29,17 @@ namespace TalentHub
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(871, 604);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
