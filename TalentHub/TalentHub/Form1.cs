8a9764775c6b2a240cad29ef4bf2d5f8f03eb7c9
DiegoBautista <dabautistac@correo.url.edu.gt>
2022-11-09 12:04:29 -0600

    RSA algorithim corrected, use BigInteger; and add references to the projects

--- TalentHub/TalentHub/Form1.cs
+++ TalentHub/TalentHub/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using ClassLibrary.CODING;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,7 @@ namespace TalentHub
         public Form1()
         {
             InitializeComponent();
+            
         }
     }
 }
