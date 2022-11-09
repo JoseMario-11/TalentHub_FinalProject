e634dba20469eced280686124b17c08dcf1acbcc
danieelfcr <danielfcab2002@gmail.com>
2022-11-09 08:26:11 -0600

    God please save us 2

--- TalentHub/TalentHub/Form1.Designer.cs
+++ TalentHub/TalentHub/Form1.Designer.cs
@@ -30,24 +30,37 @@ namespace TalentHub
         private void InitializeComponent()
         {
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(111, 126);
+            this.label2.Location = new System.Drawing.Point(194, 228);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.Size = new System.Drawing.Size(143, 29);
             this.label2.TabIndex = 0;
             this.label2.Text = "Hola mundo";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(206, 301);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 29);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Hola mundo 2";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(498, 333);
+            this.ClientSize = new System.Drawing.Size(872, 604);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -59,6 +72,7 @@ namespace TalentHub
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
