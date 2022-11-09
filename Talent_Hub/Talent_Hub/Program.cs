b2d460c4c3b3e4f7ebee265c88a5dd30bd0d364c
DiegoBautista <da10bautista@gmail.com>
2022-11-09 07:25:00 -0600

    Change of the solution, for incompatibilty

--- Talent_Hub/Talent_Hub/Program.cs
+++ Talent_Hub/Talent_Hub/Program.cs
@@ -1,16 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace Talent_Hub
 {
-    internal static class Program
+    static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
     }
