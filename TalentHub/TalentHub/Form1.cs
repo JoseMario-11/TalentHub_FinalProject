90af4436070d4bfffe6dde177f27c54429ddfd97
JoseMarold <jmmr114@gmail.com>
2022-11-11 15:54:34 -0600

    Data entry and AVL added

--- TalentHub/TalentHub/Form1.cs
+++ TalentHub/TalentHub/Form1.cs
@@ -6,15 +6,74 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using System.Windows.Forms;
+using System.IO;
+using TalentHub.AVL;
 
 namespace TalentHub
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        //Elements used to open a .csv file with information about applicant records
+        OpenFileDialog pathSearcher = new OpenFileDialog();
+        FileStream file;
+        StreamReader read;
+        public AVLclass AVL = new AVLclass();
+
+
+        // Function dedicated to convert a string into json format  
+        public void JsonConverter(StreamReader file)
+        {
+            //while the end of the stream is not reached, take one line of the file content
+            while (!file.EndOfStream)
+            {
+                string line = file.ReadLine().Trim();
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    //array that contains the necessary instructions to create the AVL with all the applicant records
+                    string[] instructions = line.Split(';');
+                    instructions[0].ToLower();
+
+                    //deserialize string with newtonsoft.json to create a new applicant element
+                    Applicant applicant = JsonConvert.DeserializeObject<Applicant>(instructions[1]);
+
+                    //the first position of the array indicates what kind of operation the AVL needs to do to generate its structure
+
+                    switch (instructions[0].ToLower())
+                    {
+                        case "insert":
+                            AVL.Root = AVL.Insert(AVL.Root, applicant);
+                            break;
+                        case "patch":
+                            AVL.Edit(AVL.Root, applicant);
+                            break;
+                        case "delete":
+                            AVL.Delete(AVL.Root, applicant);
+                            break;
+                    }
+
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pathSearcher.Filter = "CSV files |*.csv";
+            if (pathSearcher.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = pathSearcher.FileName;
+                file = new FileStream(pathSearcher.FileName, FileMode.OpenOrCreate);
+                read = new StreamReader(file);
+                JsonConverter(read);
+            }
         }
     }
 }
